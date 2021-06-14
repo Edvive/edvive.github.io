5837c8ccf3f2ecd1d060ad2541d003c1ce1ee046
Mushfiqur Rahman <iam1mahin@gmail.com>
2021-06-15 01:56:47 +0600

    Dynamic Links

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -7,7 +7,42 @@ Verb Activity      Subject
 
 */
 
+const SPREADSHEET_ID = "1VVevCDW91LZd2Yv_zMYHEOMQypheG6WQM5VQUf5jAcY";
 
+var lightState = 0;
+
+window.onload = function() {
+    // let ajax = new XMLHttpRequest();
+
+    $.ajax({
+        url: `http://gsx2json.com/api?id=${SPREADSHEET_ID}&columns=false`
+    }).done(function(data) {
+        data = data.rows[0];
+
+        for(button of document.getElementById("redirects").children) {
+            button.setAttribute("href", data[button.dataset.ref]);
+        }
+    }).always(() => {
+        for(button of document.getElementById("redirects").children) {
+            button.classList.remove("disabled-link");
+        }
+    })
+
+    // ajax.onload = function() {
+    //     if(ajax.status == ajax.DONE && ajax.readyState == ajax.)
+    // }
+}
+
+document.getElementById("lights").addEventListener('change', (ev) => {
+    // document.body.classList.add("bright-body");
+    // ev.target.checked ? document.html.classList.add("bright-body") : document.html.classList.remove("bright-body");
+    ev.target.checked ? document.body.classList.add("bright-body") : document.body.classList.remove("bright-body");
+    
+    // console.log(Array.from(document.getElementsByClassName("btn")))
+    for (button of document.getElementsByClassName("btn")) {
+        ev.target.checked ? button.classList.add("bright-btn") : button.classList.remove("bright-btn");
+    }
+})
 
 Array.prototype.pickRandom = function () {
     return this[Math.floor(Math.random() * this.length)];
